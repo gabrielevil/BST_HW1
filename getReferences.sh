2b13e236abcdf50d84ba789f78becd9b0c4daa74
Gabrielė Vilutytė <99329927+gabrielevil@users.noreply.github.com>
2023-03-09 16:36:05 +0200

    Update getReferences.sh

--- getReferences.sh
+++ getReferences.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 #Download the reference genome in FASTA format
 wget "https://ftp.ensembl.org/pub/release-109/fasta/mus_musculus/dna/Mus_musculus.GRCm39.dna.toplevel.fa.gz"
-mv Mus_musculus.GRCm39.dna.toplevel.fa.gz HW1/refs
+
 #Download the reference transcriptome in FASTA format
 wget "https://ftp.ensembl.org/pub/release-109/fasta/mus_musculus/cdna/Mus_musculus.GRCm39.cdna.all.fa.gz"
-mv Mus_musculus.GRCm39.cdna.all.fa.gz HW1/refs
+
 #Download GTF/GFF3 file
 wget "https://ftp.ensembl.org/pub/release-109/gtf/mus_musculus/Mus_musculus.GRCm39.109.gtf.gz"
-mv Mus_musculus.GRCm39.109.gtf.gz HW1/refs
+
 #Download raw FASTQ files
-fasterq-dump --split-files SRR8985047
-fasterq-dump --split-files SRR8985048
-fasterq-dump --split-files SRR8985049
-fasterq-dump --split-files SRR8985050
-mv SRR* HW1/inputs
+prefetch SRR8985047 SRR8985048 SRR8985049 SRR8985050
+
+fastq-dump --gzip SRR8985047 SRR8985048 SRR8985049 SRR8985050
+#Move to correct directories
+mv SRR* ~/HW1/inputs
+mv Mus* ~/HW1/refs
+mv fasterq* ~/HW1/refs
