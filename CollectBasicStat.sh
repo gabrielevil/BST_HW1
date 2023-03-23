66bf13d7db5cfdfb2e2985c0800f2b1ce294545d
Gabrielė Vilutytė <99329927+gabrielevil@users.noreply.github.com>
2023-03-23 16:31:53 +0200

    Update CollectBasicStat.sh

--- CollectBasicStat.sh
+++ CollectBasicStat.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 #Calculate (and print to the screen) the number of sequences in your reference genome
 echo "number of sequences: " 
-grep -c "^>" ~/HW1/refs/Mus_musculus.GRCm39.dna.toplevel.fa.gz
+grep -c "^>" ~/HW1/refs/mm10dna.fa.gz
 
 #Calculate (and print to the screen) the number of reads in each sample.
 echo "number of reads in each sample: "
-grep -c "^@.*$" ~/HW1/inputs/SRR8985047.fastq.gz
-grep -c "^@.*$" ~/HW1/inputs/SRR8985048.fastq.gz
-grep -c "^@.*$" ~/HW1/inputs/SRR8985049.fastq.gz
-grep -c "^@.*$" ~/HW1/inputs/SRR8985050.fastq.gz
+grep -c "^@.*$" ~/HW1/inputs/*.fastq.gz
 
 #Calculate the number of protein-coding genes in your genome. (gtf)
 echo "number of protein-coding genes: " 
